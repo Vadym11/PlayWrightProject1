1d32ace34c80a5ea6e09f6b04615014002147944
Vadym Tymeichuk <40869955+Vadym11@users.noreply.github.com>
2025-06-17 12:01:57 +0100

    beforeEach added

--- tests/workshop_9/workshop_9.spec.ts
+++ tests/workshop_9/workshop_9.spec.ts
@@ -1,30 +1,33 @@
 import {test, expect} from '@playwright/test';
 
-test('Automating form submission in @githubActions', async ({page}) => {
+test.describe('Running test with GitHub Actions', () => {
 
-    await page.goto('https://demo.playwright.dev/todomvc/');
+    test.beforeEach(async ({page}) => {
+        await page.goto('https://demo.playwright.dev/todomvc/');
+    })
 
-    const newToDo = page.getByPlaceholder('What needs to be done?');
-    await newToDo.fill('John Doe');
-    await newToDo.press('Enter');
-    await newToDo.fill('John Doe 1');
-    await newToDo.press('Enter');
+    test('Automating form submission in @githubActions', async ({page}) => {
 
-    const firstToDo = page.getByTestId('todo-item').nth(0);
-    await firstToDo.getByRole('checkbox').check();
-    const secondToDo = page.getByTestId('todo-item').nth(1);
+        const newToDo = page.getByPlaceholder('What needs to be done?');
+        await newToDo.fill('John Doe');
+        await newToDo.press('Enter');
+        await newToDo.fill('John Doe 1');
+        await newToDo.press('Enter');
 
-    await expect(firstToDo).toHaveClass('completed');
-    await expect(secondToDo).not.toHaveClass('completed');
-})
+        const firstToDo = page.getByTestId('todo-item').nth(0);
+        await firstToDo.getByRole('checkbox').check();
+        const secondToDo = page.getByTestId('todo-item').nth(1);
 
-test('Handling form in @githubActions', async ({page}) => {
+        await expect(firstToDo).toHaveClass('completed');
+        await expect(secondToDo).not.toHaveClass('completed');
+    })
 
-    await page.goto('https://demo.playwright.dev/todomvc/');
+    test('Handling form in @githubActions', async ({page}) => {
 
-    await page.fill('[placeholder="What needs to be done?"]', 'John Doe');
-    await page.locator('[placeholder="What needs to be done?"]').press('Enter');
+        await page.fill('[placeholder="What needs to be done?"]', 'John Doe');
+        await page.locator('[placeholder="What needs to be done?"]').press('Enter');
 
-    const checkbox = page.locator('.toggle')
-    await checkbox.check();
+        const checkbox = page.locator('.toggle')
+        await checkbox.check();
+    })
 })
